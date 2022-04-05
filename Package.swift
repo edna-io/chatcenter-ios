dedd7edc4d263e69cba5af01cdc2fb2729667342
jenkins <jenkins@edna.ru>
2022-04-05 11:57:57 +0300

    Release 4.0.5

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EDNAChatCenter",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Threads",
+            targets: ["Threads"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "Threads",
+            url: "https://github.com/edna-io/chatcenter-ios/releases/download/4.0.5/threads-4.0.5.zip",
+            checksum: "02a4c7c65b1cff54ba9730743b5996c7bb61558639b044579686882062cfa9e9"
+        ),
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# chatcenter-ios
+# edna ChatCenter SDK for iOS
+ChatCenter SDK
+
+https://edna.ru/
+
+По вопросам продаж/Sales:<br>
+sales@edna.ru
+
+Техническая поддержка/Tech support:<br>
+support@edna.ru
