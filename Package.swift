71b807b74a9595c9f95148bf44e9c0ca63838b3c
jenkins <jenkins@edna.ru>
2024-05-17 08:57:03 +0300

    Release 4.32.0

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Threads",
-            url: "https://github.com/edna-io/chatcenter-ios/releases/download/4.31.0/threads-4.31.0.zip",
-            checksum: "bf83e589b0ab631950466ac575a57fc37fccf3d92981a5d4196baf72d78b39e6"
+            url: "https://github.com/edna-io/chatcenter-ios/releases/download/4.32.0/threads-4.32.0.zip",
+            checksum: "0f7dbbcb8e0ce6365e121fc5cfb90fe9e51e53901635671fa66cf0001aa055e8"
         ),
     ]
 )
